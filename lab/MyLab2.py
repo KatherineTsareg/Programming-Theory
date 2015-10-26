6720aba86c4c14a185f69ffbd1664ffa0e506299
Katherina Tsaregorodtseva <tennleston@gmail.com>
2015-10-27 02:50:02 +0400

    исправлены ошибки, теперь все работает правильно

--- lab/MyLab2.py
+++ lab/MyLab2.py
@@ -1,21 +1,46 @@
+#-*- coding: utf8 -*-
 import urllib
 import urllib2
 import re
+import os
 
 
-list_of_extensions = ['gif', 'bmp', 'jpg', 'jpeg', 'png', 'js', 'css']
+list_of_extensions = ['gif', 'bmp', 'jpg', 'jpeg', 'png', 'js', 'css', 'html', 'jsp', 'scn', 'ico']
+my_dir = os.getcwd()
 
 
 url = 'http://lenta.ru'
+extra_content = ''
+nameTheFolder = url[url.find('/') + 2 : ] + '_files' #создаем имя для папки, куда будем сохранять
+os.mkdir(my_dir + '\\' + nameTheFolder)
+os.chdir(my_dir + '\\' + nameTheFolder)
+
 content = urllib2.urlopen(url).read()
 img_urls = re.findall('img.*?src="(.*?)"', content)
-js_urls = re.findall('script.*?src="(http:*.*?)"', content)
+img_urls1 = re.findall('href="(.*?)"', content)
+js_urls = re.findall('script.*?src=\"(.*?.js)\"', content)
+js_urls1 = re.findall('link.*?href=\"(.*?.js)\"', content)
 css_urls = re.findall('link.*?href=\"(.*?.css)\"',content)
-urls = img_urls + js_urls + css_urls
-print urls
+urls = img_urls + js_urls + css_urls + js_urls1 + img_urls1
+   
 for i in range(len(urls)):
-    address = urls[i]
-    if address[address.rfind('.') + 1 : ] in list_of_extensions:
-        name_file = address[address.rfind('/') + 1 : ]
-        urllib.urlretrieve(address, name_file)
+    try:
+        address = urls[i]
+        if address[address.rfind('.') + 1 : ] in list_of_extensions:
+                name_file = address[address.rfind('/') + 1 : ]
+                #print address
+                index = content.find(address)
+                extra_content = content[ : index] + './' + name_file + content[index + len(address):]
+                content = extra_content
+                extra_content = ''
+                if address.find('http') < 0:
+                    address = url + address
+                if address.find('http') > 0:
+                    address = address[address.find('http') : ]
+                urllib.urlretrieve(address, name_file)
+    except  IOError:
+        print 'Error'
+fout = open(url[url.find('//') + 2 : ] + '.html', 'w')
+fout.write(content)
+fout.close()
 print 'end'
--- lab/MyLab3.py
+++ lab/MyLab3.py
@@ -4,41 +4,90 @@ import urllib2
 import re
 import os
 
-
-list_of_extensions = ['gif', 'bmp', 'jpg', 'jpeg', 'png', 'js', 'css']
-urls_list = [] 
-#url = raw_input('Write url: ')
-#word = raw_input('Write the word: ')
-url = 'http://lenta.ru'
-word = 'СМИ'
-content_txt = open('content.txt','w')
-content_txt.write(urllib2.urlopen(url).read())
-content_txt.close()
-fin = open('content.txt', 'r')
-for line in fin:
-    if line.find(word) >= 0:
-        word_url = url + line[line.find('href="') + 6 : line.rfind('">')]
-        if word_url not in urls_list:
-            urls_list.append(word_url)
+list_of_extensions = ['gif', 'bmp', 'jpg', 'jpeg', 'png', 'js', 'css', 'html', 'ico']
 my_dir = os.getcwd()
-count = 1
-for url in urls_list:
-    new_dir = my_dir + '\\' + str(count)
-    count += 1;
+numb_of_pages = 100
+
+
+def SaveRecourcesUrl(main_url, url, count, my_dir):
+    extra_content = ''
+    nameTheFolder = str(count) #создаем имя для папки, куда будем сохранять
+    new_dir = my_dir + '\\' + nameTheFolder
     os.mkdir(new_dir)
     os.chdir(new_dir)
-    content = urllib2.urlopen(url).read()
+    if url.find('http') < 0:
+        correct_url = main_url + url
+    else:
+        correct_url = url
+    content = urllib2.urlopen(correct_url).read()
     img_urls = re.findall('img.*?src="(.*?)"', content)
-    js_urls = re.findall('script.*?src="(http:*.*?)"', content)
+    img_urls1 = re.findall('href="(.*?)"', content)
+    js_urls = re.findall('script.*?src=\"(.*?.js)\"', content)
+    js_urls1 = re.findall('link.*?href=\"(.*?.js)\"', content)
     css_urls = re.findall('link.*?href=\"(.*?.css)\"',content)
-    urls = img_urls + js_urls + css_urls
-    for j in range(len(urls)):
-        address = urls[j]
-        if address[address.rfind('.') + 1 : ] in list_of_extensions:
-            name_file = address[address.rfind('/') + 1 : ]
-            urllib.urlretrieve(address, name_file)
-    fout = open('text_url.html', 'w')
+    urls = img_urls + js_urls + css_urls + js_urls1 + img_urls1
+    #print urls
+    for i in range(len(urls)):
+        try:
+            address = urls[i]
+            if address[address.rfind('.') + 1 : ] in list_of_extensions:
+                name_file = address[address.rfind('/') + 1 : ]
+                index = content.find(address)
+                extra_content = content[ : index] + './' + name_file + content[index + len(address):]
+                content = extra_content
+                extra_content = ''
+                if address.find('http') < 0:
+                    address = main_url + address
+                if address.find('http') > 0:
+                    address = address[address.find('http') : ]
+                urllib.urlretrieve(address, name_file)
+        except  IOError:
+            print 'Error'
+    fout = open(str(count) + '.html', 'w')
     fout.write(content)
     fout.close()
-fin.close()
-print 'end'
+
+
+      
+urls_list = []
+savedPages = []
+count = 1
+working = True
+
+main_url = 'http://lenta.ru'
+word = 'Медведев'
+content = urllib2.urlopen(main_url).read()
+urls_list = re.findall('a.*?href="(.*?)"',content)
+i = 0
+while (i < len(urls_list)) and (len(savedPages) <= numb_of_pages):
+    url = urls_list[i]
+    if url.find('http') < 0:
+        correct_url = main_url + url
+    else:
+        correct_url = url
+    if (url.find('@') > 0):
+        urls_list.pop(i)
+        working = False
+    if working:
+        #print correct_url
+        content = urllib2.urlopen(correct_url).read()
+        if (content.find(word) > 0) and (correct_url not in savedPages) and (len(savedPages) <= numb_of_pages) and (correct_url != (main_url +'/rss')):
+            try:
+                SaveRecourcesUrl(main_url, url, count, my_dir)
+                os.chdir(my_dir)
+                count += 1
+                savedPages.append(correct_url)
+                if len(urls_list) <= 1000:
+                    urls_list += re.findall('a.*?href="(.*?)"',content)
+                urls_list.pop(i)
+            except  IOError:
+                urls_list.pop(i)
+        elif (content.find(word) < 0) and (correct_url not in savedPages):
+            if len(urls_list) <= 1000:
+                urls_list += re.findall('a.*?href="(.*?)"',content)
+            urls_list.pop(i)
+        elif ((content.find(word) > 0) and (correct_url in savedPages)) or (correct_url == (main_url +'/rss')):
+            urls_list.pop(i)
+    working = True
+print 'End'
+
