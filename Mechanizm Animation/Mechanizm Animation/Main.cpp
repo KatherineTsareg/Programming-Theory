0f8ebe393e3cd0e9b54ebba9c0b327efda2d567c
KatherineTsareg <tennleston@gmail.com>
2015-12-22 02:12:07 +0300

    Lab 7.3

---
+++ Mechanizm Animation/Mechanizm Animation/Main.cpp
@@ -0,0 +1,50 @@
+#include <SFML/Graphics.hpp> 
+#include "pendulum.h"
+#include "const.h"
+
+using namespace sf;
+
+void draw_elements(RenderWindow &window)
+{
+	window.clear(Color::White);
+
+	window.draw(pendulum.base);
+	window.draw(pendulum.left_arrow);
+	window.draw(pendulum.right_arrow);
+
+	window.draw(pendulum.left_cogwheel);
+	window.draw(pendulum.right_cogwheel);
+
+	window.display();
+}
+
+void start_move(RenderWindow &window)
+{
+	pendulum.init_elements_of_pendulum();
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		pendulum.update();
+		draw_elements(window);
+	}
+}
+
+int main() {
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	Texture cogwheels;
+	cogwheels.loadFromFile("cogwheel.psd");
+	pendulum.left_cogwheel.setTexture(&cogwheels);
+	pendulum.right_cogwheel.setTexture(&cogwheels);
+
+	RenderWindow window(VideoMode(WINDOW_SIZE.x, WINDOW_SIZE.y), "Pendulum", Style::Default, settings);
+	start_move(window);
+	return 0;
+}
+
---
+++ Mechanizm Animation/Mechanizm Animation/const.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML/Graphics.hpp> 
+
+using namespace sf;
+
+const Vector2f WINDOW_SIZE = { 900, 600 };
+const Vector2f INITAL_POSITION_LEFT_COGWHEEL = { 220, 160 };
+const Vector2f INITAL_POSITION_RIGHT_COGWHEEL = { 362, 372 };
+const Vector2f INITAL_POSITION_PENDULUM = { 580, 290 };
